cf6a725d73579bda1ac8e98ee8e7002d930ac1bb
thaiphuc2203 <thaiphuc2203@gmail.com>
2022-05-22 21:57:01 +0700

    init project

---
+++ app/graphql/mutations/create_user.rb
@@ -0,0 +1,27 @@
+class Mutations::CreateUser < Mutations::BaseMutation
+  argument :name, String, required: true
+  argument :email, String, required: true
+
+  field :user, Types::UserType, null: false
+  field :errors, [String], null: false 
+
+  def resolve(name:, email:)
+    user = User.new(name: name, email: email)
+    if user.save
+      {
+        user: user,
+        errors: []
+      }
+    else
+      {
+        user: nil,
+        errors: user.errors.full_messages
+      }
+    end
+  {
+    user: nil,
+    errors: []
+  }
+  end
+
+end
